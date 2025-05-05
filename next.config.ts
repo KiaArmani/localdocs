e047cbfa19d580ef76d4676c6ed3d0f113799263
Kia Armani <kia@kia.dev>
2025-05-05 21:23:00 +0200

    feat: configure webpack watch options

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpackDevMiddleware: (config) => {
+    // Important: return the modified config
+    config.watchOptions = {
+      ignored: ['**/node_modules/**', '**/.next/**', '**/.content-collections/**'], // Added .content-collections
+      poll: 1000, // Check for changes every second
+    };
+    return config;
+  },
+};
 
 export default withContentCollections(nextConfig);
